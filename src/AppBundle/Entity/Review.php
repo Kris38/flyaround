0927331d6db95443f1df2ab54e26e8ccb5b04a37
christophe_p <chrispichon38@gmail.com>
2017-12-06 11:24:42 +0100

    Quete relations entités

---
+++ src/AppBundle/Entity/Review.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Review
+ *
+ * @ORM\Table(name="review")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ReviewRepository")
+ */
+class Review
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var text
+     *
+     * @ORM\Column(name="text", type="text", length=255)
+     */
+    private $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="publicationDate", type="string", length=255)
+     */
+    private $publicationDate;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="note", type="smallint")
+     */
+    private $note;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User", inversedBy="usersRated")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $userRated;
+
+    /**
+    * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User", inversedBy="reviewsAuthor")
+    * @ORM\JoinColumn(nullable=false)
+    */
+    private $reviewAuthor;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Review
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set publicationDate
+     *
+     * @param string $publicationDate
+     *
+     * @return Review
+     */
+    public function setPublicationDate($publicationDate)
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publicationDate
+     *
+     * @return string
+     */
+    public function getPublicationDate()
+    {
+        return $this->publicationDate;
+    }
+
+    /**
+     * Set note
+     *
+     * @param integer $note
+     *
+     * @return Review
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return int
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+}
+
